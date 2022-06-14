54a33b30fd26682607865a4e0ee7497eb17cda37
Zach Stall <zstall4@gmail.com>
2022-06-13 22:12:11 -0600

    working on classes

--- chap.py
+++ chap.py
@@ -19,10 +19,11 @@ import sys
 import psycopg2
 import os
 from config import config
+from create_chappy_db import executeInserts
 
 
 class User:
-
+    """ Class User is the main class to create a user and update the DB"""
     def __init__(self, user_id, fname, lname, phone, email, username, password, org_ids, date_created, date_updated, admin, super_user, deleted):
         self.user_id = user_id
         self.fname = fname
@@ -41,10 +42,36 @@ class User:
 
 
     def assign_org(self, org):
+        """ 
+            Assign an org attribute to a user.
+            Org must already exist in the DB.
+            Orgs in the DB already assigned will be ignored.  
+        """
+        # get the unique org from the db
         org_id = query_chappy("SELECT org_id FROM orgs WHERE org_name = '" + org + "';")
-        query_chappy("UPDATE orgs SET user_ids = user_ids || '{\"" + self.user_id + "\"}' WHERE org_id = '" + org_id[0][0] + "';")
-        query_chappy("UPDATE users SET org_ids = org_ids || '{\"" + org_id[0][0] + "\"}' where user_id = '" + self.user_id + "';")
-        self_org_ids = org_
+    
+        # if no org, add it directory
+        if self.org_ids == None:
+            query_chappy("UPDATE orgs SET user_ids = user_ids || '{\"" + self.user_id + "\"}' WHERE org_id = '" + org_id[0][0] + "';")
+            query_chappy("UPDATE users SET org_ids = org_ids || '{\"" + org_id[0][0] + "\"}' where user_id = '" + self.user_id + "';")
+            self.org_ids = org_id[0][0]
+        # if there are already orgs assigned, check if the org id is already assigned
+        elif len(self.org_ids) >= 1 and org_id[0][0] in self.org_ids:
+            print("Org Id Already Assigned: " + str(org_id[0][0] in self.org_ids))
+        # if multiple orgs are assigned, append the new org
+        else:
+            print("Appending Org ID to User:")
+            query_chappy("UPDATE orgs SET user_ids = user_ids || '{\"" + self.user_id + "\"}' WHERE org_id = '" + org_id[0][0] + "';")
+            query_chappy("UPDATE users SET org_ids = org_ids || '{\"" + org_id[0][0] + "\"}' where user_id = '" + self.user_id + "';")
+            self.org_ids.append(org_id[0][0])
+
+    def add_user_to_db(self, psswrd):
+        executeInserts("'"+self.fname+"'", "'"+self.lname+"'", "'"+self.phone+"'", "'"+self.email+"'", "'"+self.username+"'", "crypt('"+psswrd+"', gen_salt('bf', 8))","current_timestamp","current_timestamp", self.admin, self.super_user)
+
+#class Org:
+ #   def __init__(self, org_id, org_name, date_created, user_ids, org_deleted, date_udpated):
+
+            
 
 def query_chappy(query):
      """ function to connect to the DB and run queries """
@@ -59,11 +86,14 @@ def query_chappy(query):
 
         # create a cursor
         cur = conn.cursor()
-
+        print("******************************")
+        print("Executing:")
         print(query)
+        print("******************************")
         cur.execute(query)
         conn.commit()
         
+
 
         db_query = cur.fetchall()
         return(db_query) 
@@ -75,8 +105,6 @@ def query_chappy(query):
         if conn is not None:
             cur.close()
             conn.close()
-            print('Database connection closed.')
-            print('Queries Executed Succesfully.')
 
 def create_user(username, password):
     """ Given a username and password query the db to see if that user exists. If so 
@@ -91,10 +119,22 @@ def create_user(username, password):
 
 def main():
 
-    u = create_user("sstall", "password")
-    u.assign_org("stall_house")
 
-    print("User: " + u.username)
-    print("Org: " + u.org_ids)
+
+    
+
+    #u = User('','Ollie','Stall', '555-555-5555', 'ostall@noreplay.com','ostall', 'password', '','','','False','False','False')
+    #u.add_user_to_db('password')
+    
+    new_usr = create_user("ostall", "password")
+    #new_usr.assign_org("kelly_house")
+    new_usr.assign_org("stall_house")
+    new_usr.assign_org("kelly_house")
+    
+    
+    print("User: " + new_usr.username)
+    print(new_usr.org_ids)
+    
+
 if __name__ == '__main__':
     main()
